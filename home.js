bf673cbfe872c01ebad353fb939c52f770fff6a9
Solomon Park <solomon.m.park@gmail.com>
2021-03-09 12:15:30 -0500

    Added copy, fixed navlinks

--- home.js
+++ home.js
@@ -2,15 +2,15 @@ var $window   = $(window),
 	height    = $window.height(),
 	width     = $window.width();
 
-  function sticky(){
+function sticky(){
 	var scrollTop = $window.scrollTop();
 	if (scrollTop > (height - $('#nav_wrap').height())) {
-	  $('#nav_wrap').addClass('sticky');
-	  $('#nav_wrap').removeClass('not_sticky');
+		$('#nav_wrap').addClass('sticky');
+		$('#nav_wrap').removeClass('not_sticky');
 	} else {
-	  $('#nav_wrap').removeClass('sticky');
-	  $('#nav_wrap').addClass('not_sticky');
+		$('#nav_wrap').removeClass('sticky');
+		$('#nav_wrap').addClass('not_sticky');
 	}
-  } 
-  $window.on('touchmove', sticky);
-  $window.on('scroll', sticky);  
+} 
+$window.on('touchmove', sticky);
+$window.on('scroll', sticky); 
